9625449320fa925517ba0f2715bcf3e914cbe0eb
Philippe Bruhat (BooK) <book@cpan.org>
2017-07-18 17:17:56 +0200

    Update copyright date

--- lib/Task/Git/Repository.pm
+++ lib/Task/Git/Repository.pm
@@ -66,7 +66,7 @@ Philippe Bruhat (BooK) <book@cpan.org>
 
 =head1 COPYRIGHT
 
-Copyright 2014-2015 Philippe Bruhat (BooK), All Rights Reserved.
+Copyright 2014-2017 Philippe Bruhat (BooK), All Rights Reserved.
 
 =head1 LICENSE
 
